4eeea6cb6a719f0fd8f0d06562b385ad37f9e297
Cewerty <88933231+Cewerty@users.noreply.github.com>
2026-03-20 12:55:53 +0500

    refactor: удалил ненужный движок для генерации асинхронных сессий.

--- src/pybot/db/database.py
+++ src/pybot/db/database.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from ..core.config import settings
 
@@ -8,9 +8,3 @@ if not settings.database_url:
     raise ValueError("Database URL is not configured.")
 
 engine = create_async_engine(settings.database_url, echo=False)
-
-SessionLocal = async_sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-)
