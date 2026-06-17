7f97b3142cc81538c3fd18b400b22c4b8dd65c05
Cewerty <88933231+Cewerty@users.noreply.github.com>
2026-06-17 14:23:40 +0500

    chore: добавил директорию с тестами в билд для контейнеризованных тестов

--- scripts/ci/run_container_test_coverage.sh
+++ scripts/ci/run_container_test_coverage.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
 
+export PYBOT_TEST_DATABASE_URL="postgresql+asyncpg://${POSTGRES_USER:?POSTGRES_USER must be set}:${POSTGRES_PASSWORD:?POSTGRES_PASSWORD must be set}@postgres:5432/${POSTGRES_DB:?POSTGRES_DB must be set}_test"
 sh scripts/ci/ensure_compose_test_database.sh
 uv run pytest --cov=src/pybot --cov-report=term-missing --cov-report=xml --cov-fail-under=80
